4fae8613671451e8cbe23ea8922f32a3b0bd15d9
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2020-11-15 01:48:30 -0800

    Update clock.js

--- JS/clock.js
+++ JS/clock.js
@@ -1,76 +1,78 @@
-//Selectors
-const time = document.getElementById('time');
-const greeting = document.getElementById('greeting');
-const name = document.getElementById('name');
-const date = document.getElementById('date');
-//Event Handler
+//Selector
+
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+//EVENT LISTENER
 name.addEventListener("keypress", setName);
 name.addEventListener("blur", setName);
 
-
-//Functions
+//FUNCTION
 function showtime() {
-    let today = new Date();
-    let hour = today.getHours();
-    let min = today.getMinutes();
-    let sec = today.getSeconds();
-    let todaydate = today.toDateString();
-    const amPm = hour > 12 ? 'PM' : 'AM';
-    hour = hour % 12 || 12;
-    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${amPm}`;
-    date.innerHTML = `${todaydate}`;
-    setTimeout(showtime, 1000);
+  let today = new Date();
+  let hour = today.getHours();
+  let min = today.getMinutes();
+  let sec = today.getSeconds();
+  let todaydate = today.toDateString();
+
+  // am pm format
+
+  const amPm = hour > 12 ? " PM" : " AM";
+
+  //12hour format logic
+  hour = hour % 12 || 12;
+
+  //output time
+  time.innerHTML = `${addZero(hour)}<span>:<span/>${addZero(
+    min
+  )}<span>:<span/>${addZero(sec)}${amPm}`;
+
+  setTimeout(showtime, 1000);
 }
 
 function addZero(n) {
-    return (parseInt(n, 10) < 10 ? "0" : "") + n;
+  return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
 
 function setGreeting() {
-    let today = new Date();
-    let hour = today.getHours();
-    if (hour < 12) {
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundImage = 'url("../images/morning.jpg")';
-        greeting.innerHTML = "Good Morning";
-        document.body.style.color = "white";
+  let today = new Date();
+  let hour = today.getHours();
 
-    } else if (hour < 18) {
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundImage = 'url("../images/afternoon.jpg")';
-        greeting.innerHTML = "Good Afternoon";
-
-    } else {
-        document.body.style.backgroundSize = "cover";
-        document.body.style.backgroundImage = 'url("../images/evening.jpg")';
-
-        greeting.innerHTML = "Good Evening";
-        document.body.style.color = "white";
-    }
-
-
+  if (hour < 12) {
+    document.body.style.backgroundImage = 'url("images/morning.jpg")';
+    greeting.innerHTML = "Good Morning";
+  } else if (hour < 18) {
+    document.body.style.backgroundImage = 'url("images/afternoon.jpg")';
+    greeting.innerHTML = "Good Afternoon";
+  } else {
+    document.body.style.backgroundImage = 'url("images/night.jpg")';
+    greeting.innerHTML = "Good Evening";
+    document.body.style.color = "white";
+  }
 }
+//Function call
 
 function getName() {
-    if (localStorage.getItem("mykeyname") == null) {
-        name.innerHTML = '[Enter name]';
-    } else {
-        name.innerHTML = localStorage.getItem("mykeyname");
-    }
+  if (localStorage.getItem("myData") == null) {
+    name.innerHTML = "[Prachi Bansal]";
+  } else {
+    name.innerHTML = localStorage.getItem("myData");
+  }
 }
 
 function setName(e) {
-    if (e.type === "keypress") {
-        if (e.keyCode == 13) {
-            localStorage.setItem("mykeyname", e.target.innerHTML);
-            name.blur();
-        }
-    } else {
-        localStorage.setItem("mykeyname", e.target.innerHTML);
+  if (e.type === "keypress") {
+    if (e.keyCode == 13) {
+      localStorage.setItem("myData", e.target.innerHTML);
+      name.blur();
     }
+  } else {
+    localStorage.setItem("myData", e.target.innerHTML);
+    // e.target means name and the be store the innerhtml of name into mydata database which get store into localstorage
+  }
 }
-
-//Functions call
+getName();
 showtime();
 setGreeting();
-getName();
